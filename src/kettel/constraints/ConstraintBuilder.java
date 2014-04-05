e4705c3f067cc8ece3d8e811397d98e9b111dfc4
Miguel Guimarães <hmtlguimaraes@gmail.com>
2014-04-05 20:39:36 +0100

    some brief changes

--- src/kettel/constraints/ConstraintBuilder.java
+++ src/kettel/constraints/ConstraintBuilder.java
@@ -9,33 +9,36 @@ import cpn.Page;
 public abstract class ConstraintBuilder {
 
     protected Constraint constraint;
-    
+
     /**
-     * Get the {@code Constraint} builded
-     * @return A {@code Constraint} 
+     * Get the {@link Constraint} builded
+     *
+     * @return A {@code Constraint}
      */
     public Constraint getConstraint() {
         return this.constraint;
     }
 
-    /** 
-     * Create a new {@code Constraint}
+    /**
+     * Create a new {@link Constraint}
      */
     public void createConstraint() {
         this.constraint = new Constraint();
     }
 
     /**
-     * Abstract method to be implemented in each specific constraint
-     * Initialize each constraint with the keywords
+     * Abstract method to be implemented in each specific constraint. Initialize
+     * each constraint with the keywords
      */
     public abstract void buildConstraint();
-    
+
     /**
-     * Abstract method to be implemented in each specific constraint
-     * Verify if the specific {@code page} is according the pattern
-     * @param page
-     * @return {@code true} or {@code false} depending if the constraint is verify or not
+     * Abstract method to be implemented in each specific constraint. Verify if
+     * the specific {@code page} parameter is according the pattern
+     *
+     * @param page CPN Page submitted to dissection
+     * @return {@code true} or {@code false} depending if the constraint is
+     * verify or not
      */
     public abstract boolean verifyConstraint(Page page);
 }
